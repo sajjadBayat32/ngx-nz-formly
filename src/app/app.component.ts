581e02e1adeaa0de04c73c05cf98856718377b3c
sajjadBayat32 <sajjad.bayat32@gmail.com>
2023-08-08 16:51:05 +0330

    our sample formly updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { FormGroup } from "@angular/forms";
 import { FormlyFieldConfig, FormlyFormOptions } from "@ngx-formly/core";
 import { FormlyCustomFieldProps } from "./formly/formly-props.model";
 
-interface FormModel {
+class FormModel {
   email: string;
+  olderThan20: boolean;
+
+  constructor(element: any) {
+    this.email = element?.email;
+    this.olderThan20 = element?.olderThan20;
+  }
 }
 
 @Component({
@@ -14,7 +20,7 @@ interface FormModel {
 })
 export class AppComponent implements OnInit {
   now = Date.now();
-  model: FormModel | {} = {};
+  model = new FormModel({});
   form = new FormGroup({});
   options: FormlyFormOptions = {};
   fields: FormlyFieldConfig<FormlyCustomFieldProps>[] = [
@@ -27,7 +33,7 @@ export class AppComponent implements OnInit {
         minLength: 3,
         label: "First Name",
         styles: {
-          labelWidth: "100px",
+          labelWidth: "110px",
           wrapperClass: "mb-4",
         },
       },
@@ -40,7 +46,7 @@ export class AppComponent implements OnInit {
         maxLength: 10,
         label: "Last Name",
         styles: {
-          labelWidth: "100px",
+          labelWidth: "110px",
           wrapperClass: "mb-4",
         },
       },
@@ -54,7 +60,7 @@ export class AppComponent implements OnInit {
         mask: "phone",
         placeholder: "e.x. 9127403028",
         styles: {
-          labelWidth: "100px",
+          labelWidth: "110px",
           wrapperClass: "mb-4",
         },
       },
@@ -68,7 +74,7 @@ export class AppComponent implements OnInit {
         label: "Email address",
         placeholder: "e.x. sajjad@gmail.com",
         styles: {
-          labelWidth: "100px",
+          labelWidth: "110px",
           wrapperClass: "mb-4",
         },
         change: data => console.log(data),
@@ -87,12 +93,38 @@ export class AppComponent implements OnInit {
         label: "Password",
         placeholder: "use complex one",
         styles: {
-          labelWidth: "100px",
+          labelWidth: "110px",
           wrapperClass: "mb-4",
         },
       },
       validators: {
         validation: ["password"],
+      },
+    },
+    {
+      key: "olderThan20",
+      type: "checkbox",
+      className: "flex-50 px-2 mb-4",
+      props: {
+        label: "I am older that 20",
+      },
+    },
+    {
+      key: "Age",
+      type: "input",
+      className: "flex-50 px-2",
+      props: {
+        mask: "00",
+        label: "Age",
+        disabled: this.model.olderThan20,
+        placeholder: "enter your age",
+        styles: {
+          labelWidth: "110px",
+          wrapperClass: "mb-4",
+        },
+      },
+      expressionProperties: {
+        "props.disabled": model => !model.olderThan20,
       },
     },
   ];
