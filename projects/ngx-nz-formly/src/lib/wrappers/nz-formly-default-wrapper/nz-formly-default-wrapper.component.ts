d659e8c129b8592547346c77a7f451f28a89cf7d
Sajjad Bayat <sajjad.bayat32@gmail.com>
2024-01-22 17:09:11 +0330

    field error animation changed from angular animations to pure css

--- projects/ngx-nz-formly/src/lib/wrappers/nz-formly-default-wrapper/nz-formly-default-wrapper.component.ts
+++ projects/ngx-nz-formly/src/lib/wrappers/nz-formly-default-wrapper/nz-formly-default-wrapper.component.ts
@@ -1,24 +1,11 @@
 import { Component } from "@angular/core";
 import { FieldTypeConfig, FieldWrapper } from "@ngx-formly/core";
-import { animate, style, transition, trigger } from "@angular/animations";
 import { NzFormlyCommonProps } from "../../ngx-nz-formly-props.model";
 
 @Component({
   selector: "app-nz-formly-default-wrapper",
   templateUrl: "./nz-formly-default-wrapper.component.html",
   styleUrls: ["./nz-formly-default-wrapper.component.scss"],
-  animations: [
-    trigger("myAnimation", [
-      transition(":enter", [
-        style({ bottom: "4px", opacity: 0 }),
-        animate("100ms", style({ bottom: "0px", opacity: 1 })),
-      ]),
-      transition(":leave", [
-        style({ bottom: "0px", opacity: 1 }),
-        animate("100ms", style({ bottom: "4px", opacity: 0 })),
-      ]),
-    ]),
-  ],
 })
 export class NzFormlyDefaultWrapperComponent extends FieldWrapper<
   FieldTypeConfig<NzFormlyCommonProps>
