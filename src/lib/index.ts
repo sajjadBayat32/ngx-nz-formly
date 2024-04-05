ec1ad4482b3e08e81f8c372f0552846ac188b23d
SajjadBayat <sajjad.bayat32@gmail.com>
2024-04-05 16:53:25 +0330

    [chor] indexing files added

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+export * from "./ngx-nz-formly.module";
+export * from "./ngx-nz-formly-props.model";
+export * from "./ngx-nz-formly-type-safe.model";
+export * from "./ngx-nz-formly-validators";
+export * from "./ngx-nz-formly.config";
+export * from "./nz-date-picker";
---
+++ src/lib/wrappers/index.ts
@@ -0,0 +1,4 @@
+export * from "./nz-formly-default-wrapper/nz-formly-default-wrapper.component";
+export * from "./nz-formly-label-wrapper/nz-formly-label-wrapper.component";
+export * from "./nz-formly-addons-wrapper/nz-formly-addons-wrapper.component";
+export * from "./wrapper.extension";
